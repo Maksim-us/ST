7dfe3ed613e925f0801f4a09d354cf85a651ce4e
Max <max.mokeyev@gmail.com>
2020-07-07 08:56:26 -0500

    Update linkind-motion.groovy

--- device-handlers/linkind-motion.groovy
+++ device-handlers/linkind-motion.groovy
@@ -12,8 +12,8 @@
  *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  *  License for the specific language governing permissions and limitations
  *  under the License.
- *  Author : jinkang zhang / jk0218.zhang@samsung.com
- *  Date : 2018-07-04
+ *  Author : MM
+ *  Date : 2020-07-07
  */
 import physicalgraph.zigbee.clusters.iaszone.ZoneStatus
 import physicalgraph.zigbee.zcl.DataType
